7ca860907c6255f8a570a761084b192529e3f237
吴振禹 <wuzhenyu.wzy@bytedance.com>
2020-01-31 14:55:15 +0800

    fix: online.py fixed
    Change-Id: Ib4f6678222eac05ce17b019997b283b17c0606d9

--- config/online.py
+++ config/online.py
@@ -7,11 +7,11 @@ DEBUG = True
 SECRET_KEY = b'#"F4Q8z\n\xec]/66241'
 
 
-USERNAME = "root"
-PASSWORD = "rootroot"
-HOST = "127.0.0.1"
+USERNAME = "coroyeah"
+PASSWORD = "coroyeahWENZHU2018"
+HOST = "rm-j6cy632s227w9w6r9.mysql.rds.aliyuncs.com"
 PORT = "3306"
-DATABASE = "test1"
+DATABASE = "lijiang"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4".format(USERNAME, PASSWORD,
                                                       HOST, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
